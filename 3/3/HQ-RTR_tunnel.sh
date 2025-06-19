5d81033b3944778c15a052e3f593ea2f044f9ae3
Andrey Ryabov <ryabov@mad-soft.ru>
2025-06-19 08:56:35 +0500

    f

---
+++ 3/3/HQ-RTR_tunnel.sh
@@ -0,0 +1,24 @@
+apt update
+apt install strongswan libcharon-extra-plugins -y
+echo -e "ip link add vti0 type vti local 172.16.4.2 remote 172.16.5.2 key 1\nip addr add 10.10.10.1/30 dev vti0\nip link set vti0 up" > /etc/ipsec.sh
+chmod +x /etc/ipsec.sh
+cat > /etc/ipsec.conf <<EOF
+conn tunnel
+	leftupdown=/etc/ipsec.sh
+	left=172.16.4.2
+	leftsubnet=0.0.0.0/0
+	right=172.16.5.2
+	rightsubnet=0.0.0.0/0
+	authby=secret
+	keyexchange=ikev2
+	auto=start
+	mark=1
+	type=tunnel
+	esp=aes256-sha256-modp1024
+EOF
+echo '172.16.4.2 172.16.5.2 : PSK "123qweR%"' > /etc/ipsec.secrets
+sed -i 's/install_routes\s*=.*/install_routes = no/' /etc/strongswan.d/charon.conf
+sed -i '/gre1/,+2d' /etc/network/interfaces
+ip tunnel del gre1
+vtysh -c 'conf t' -c 'no interface gre1'
+ipsec restart
---
+++ 3/4/NAT-firewall.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+iptables -A INPUT -i lo -j ACCEPT
+iptables -A INPUT -i eth0 -p tcp -m multiport --dports 22,53,80,443,2024 -j ACCEPT
+iptables -A INPUT -i eth0 -p udp -m multiport --dports 53,123,500,4500 -j ACCEPT
+iptables -A INPUT -i eth0 -p icmp -j ACCEPT
+iptables -A INPUT  -p esp -j ACCEPT
+iptables -A FORWARD -p esp -j ACCEPT
+iptables -A INPUT -i eth0 -m state --state NEW -j DROP
+iptables -A OUTPUT -j ACCEPT
+iptables-save > /root/rules
+iptables -L -v
+ipsec status
